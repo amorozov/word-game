aa675cae198384766d15f23bbe76910df71fcbbe
Alexey Morozov <morozov@gmail.com>
2022-02-11 22:31:28 +0700

    tmp2

--- backend-django/wordgame/models.py
+++ backend-django/wordgame/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_fsm import FSMField, transition
 
 # Create your models here.
+class Game(models.Model):
+    class State(object):
+        NEW = "NEW"
+        QUESTIONED = "QUESTIONED"
+        ANSWER_CORRECT = "ANSWER_CORRECT"
+        ANSWER_INCORRENT = "ANSWER_INCORRECT"
+        _EXPLAIN = "ANSWER_EXPLAIN"
+
+    state = FSMField(default='new', protected = True)
